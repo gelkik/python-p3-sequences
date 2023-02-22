5ed0b8ef652b5a047322ca7c008f8b09c1f94a5f
George Elkik <gelkik@gmail.com>
2023-02-21 21:02:03 -0500

    final

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        array = [0,1]
+        i=0
+        while len(array)<12:
+            num = array[i]+array[i+1]
+            array.append(num)
+            i+=1
+        print(array[0:length])
+
